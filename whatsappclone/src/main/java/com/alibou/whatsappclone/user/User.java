69c460ba30d4d4183340cc8e68d09a7c872a7a44
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-25 10:58:53 -0300

    chore: setup user

--- whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
@@ -1,6 +1,33 @@
 package com.alibou.whatsappclone.user;
 
 import com.alibou.whatsappclone.common.BaseAuditingEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+
 
 public class User extends BaseAuditingEntity {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime lastSeen;
+
+
 }
