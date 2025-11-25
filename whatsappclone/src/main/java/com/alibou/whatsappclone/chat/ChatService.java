7836e42a47960ef05658feba018e7fc45a883d29
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-25 19:56:38 -0300

    feat: create ChatService

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatService.java
@@ -0,0 +1,10 @@
+package com.alibou.whatsappclone.chat;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+}
