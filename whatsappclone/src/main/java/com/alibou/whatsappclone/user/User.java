e97408b3eb7e34acb2877e46c51e49fa0488ef21
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-25 11:36:23 -0300

    chore: update user query

--- whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
@@ -3,6 +3,7 @@ package com.alibou.whatsappclone.user;
 import com.alibou.whatsappclone.chat.Chat;
 import com.alibou.whatsappclone.common.BaseAuditingEntity;
 import jakarta.persistence.*;
+import jdk.jfr.Name;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,14 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
+
+@NamedQuery(name = UserConstants.FIND_USER_BY_EMAIL,
+            query = "SELECT u FROM  User u WHERE u.email = :email")
+@NamedQuery(name = UserConstants.FIND_ALL_USER_EXCEPT_SELF,
+            query = "SELECT u  FROM User u WHERE u.id !=:publicId")
+
+@NamedQuery(name = UserConstants.FIND_USER_BY_PUBLIC_ID,
+            query = "SELECT u  FROM User u WHERE u.id =:publicId")
 
 
 public class User extends BaseAuditingEntity {
