e5e779d11c516769f299c131fd9194838c277daa
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-25 11:17:33 -0300

    chore: update user

--- whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
@@ -1,15 +1,16 @@
 package com.alibou.whatsappclone.user;
 
+import com.alibou.whatsappclone.chat.Chat;
 import com.alibou.whatsappclone.common.BaseAuditingEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Getter
@@ -28,6 +29,21 @@ public class User extends BaseAuditingEntity {
     private String lastName;
     private String email;
     private LocalDateTime lastSeen;
+
+
+    @OneToMany(mappedBy = "sender")
+    private List<Chat> chatAsSender;
+    @OneToMany(mappedBy = "recipient")
+    private List<Chat> chatAsRecipient;
+
+
+    @Transient
+    public boolean isUserOnline(){
+    return lastSeen != null && lastSeen.isAfter(LocalDateTime.now().minusMinutes(LAST_ACTIVE_INTERVAL));
+
+
+    }
+
 
 
 }
