9a7ead3471bcb97886201f5a49e7aa7b4708fa73
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-25 11:35:57 -0300

    feat: create UserConstants

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/UserConstants.java
@@ -0,0 +1,13 @@
+package com.alibou.whatsappclone.user;
+
+public class UserConstants {
+
+    public static final String FIND_USER_BY_EMAIL = "User.findUserByEmail";
+    public static final String FIND_ALL_USER_EXCEPT_SELF ="User.findAllUserExceptSelf";
+    public static final String FIND_USER_BY_PUBLIC_ID = "User.findUserByPublicId";
+
+
+    private UserConstants(){}
+
+
+}
