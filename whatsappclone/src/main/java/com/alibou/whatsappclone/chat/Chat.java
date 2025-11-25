25423c6376be86eccd47bc92226bba4c91b0bca8
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-25 11:27:27 -0300

    chore: update chat relationships

--- whatsappclone/src/main/java/com/alibou/whatsappclone/chat/Chat.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/Chat.java
@@ -1,6 +1,8 @@
 package com.alibou.whatsappclone.chat;
 import com.alibou.whatsappclone.common.BaseAuditingEntity;
 import com.alibou.whatsappclone.message.Message;
+import com.alibou.whatsappclone.message.MessageState;
+import com.alibou.whatsappclone.message.MessageType;
 import com.alibou.whatsappclone.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -8,6 +10,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+import java.util.List
 import java.time.LocalDate;
 
 
@@ -35,5 +39,47 @@ public class Chat extends BaseAuditingEntity{
     @OneToMany(mappedBy = "chat",fetch = FetchType.EAGER)
     @OrderBy("createdDate DESC")
     private List<Message> messages;
+
+
+
+
+    @Transient
+    public String getChatName(final String senderId) {
+        if (recipient.getId().equals(senderId)){
+            return sender.getFirstName() + " " + sender.getLastName();
+        }
+        return recipient.getFirstName() + " " + recipient.getLastName();
+
+    }
+
+
+    @Transient
+    public long getUnreadMessages(final String senderId) {
+        return messages
+                .stream()
+                .filter(m -> m.getReceiverId().equals(sender))
+                .filter(m -> MessageState.SENT == m.getState())
+                .count();
+
+    }
+
+    @Transient
+    public String getLastMessage() {
+        if (messages != null && !messages.isEmpty()) {
+            if(messages.get(0).getType()!= MessageType.TEXT){
+                return "Attachment";
+            }
+            return messages.get(0).getContent();
+        }
+        return null;
+    }
+
+    @Transient
+    public LocalDateTime getLastMessageTime() {
+        if (messages != null && !messages.isEmpty())  {
+            return messages.get(0).getCreatedDate();
+        }
+        return null;
+    }
 
 }
