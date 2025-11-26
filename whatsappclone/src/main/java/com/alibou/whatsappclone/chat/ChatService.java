0ec5c42caf3b09bfad098a46401196eb64cf5b3b
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 16:36:02 -0300

    feat: update chatRepository

--- whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatService.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatService.java
@@ -2,9 +2,21 @@ package com.alibou.whatsappclone.chat;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
+
+    private final ChatRepository chatRepository;
+
+    @Transactional(readOnly = true)
+    public List<ChatResponse> getChatsByReceiverId(Authentication currentUser){
+        final String userId = currentUser.getName();
+        return chatRepository.findChatsBySenderId(userId);
+    }
 }
