510575aa27e6aa96d79a4c56cd53ddbbcfa548c3
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 17:01:46 -0300

    feat: create query find_user_by_public_id

--- whatsappclone/src/main/java/com/alibou/whatsappclone/user/UserRepository.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/UserRepository.java
@@ -8,9 +8,13 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,String> {
 
+
+
     @Query(name = UserConstants.FIND_USER_BY_EMAIL)
     Optional<User> findByEmail(@Param("email") String userEmail);
     User user = userMapper.fromTokenAttributes(token.getClaims());
 
+    @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
+    Optional<User> findByPublicId(String publicId);
 
 }
