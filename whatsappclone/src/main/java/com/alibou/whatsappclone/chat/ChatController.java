1b30c59345b371f0d7bdce8d9c233f34c95b1b47
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 17:11:43 -0300

    feat: create ResponseEntity

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatController.java
@@ -0,0 +1,28 @@
+package com.alibou.whatsappclone.chat;
+
+
+import com.alibou.whatsappclone.common.StringResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/chats")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ResponseEntity<StringResponse> createChat (
+            @RequestParam(name = "sender-id") String senderId,
+            @RequestParam(name = "receiver-id") String receiverId
+     ) {
+
+        final String chatId =chatService.createChat(senderId,receiverId);
+        StringResponse response = StringResponse.builder()
+                .response(chatId)
+                .build();
+        return ResponseEntity.ok(response);
+    }
+}
