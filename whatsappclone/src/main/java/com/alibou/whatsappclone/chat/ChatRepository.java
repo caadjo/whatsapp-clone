d159b91cf4fead53d8285f39cd7b13eb07473a28
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 16:35:54 -0300

    feat: update ChatService

--- whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatRepository.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatRepository.java
@@ -1,6 +1,15 @@
 package com.alibou.whatsappclone.chat;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 public interface ChatRepository extends JpaRepository<Chat,String> {
+    @Query(name = ChatConstants.FIND_CHAT_BY_SENDER_ID)
+    List<ChatResponse> findChatsBySenderId(@Param("senderId") String userId);
 }
