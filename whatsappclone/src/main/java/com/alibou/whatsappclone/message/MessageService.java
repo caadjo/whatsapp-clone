865d60be3a87e7fcb2515ff744008a6b1ead6750
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 17:25:59 -0300

    feat: create chatRepository.findById

--- whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageService.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageService.java
@@ -1,4 +1,23 @@
 package com.alibou.whatsappclone.message;
 
+import com.alibou.whatsappclone.chat.Chat;
+import com.alibou.whatsappclone.chat.ChatRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+    private final ChatRepository chatRepository;
+
+    public void saveMessage(MessageRequest messageRequest) {
+        Chat chat = chatRepository.findById(messageRequest.getChatId())
+                .orElseThrow(() -> new EntityNotFoundException("Chat not found"));
+
+    }
+
 }
