3af89c1db1c9ac24191d301fc45dfaa15a07689a
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 16:40:41 -0300

    feat:create chatresponse builder

--- whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatMapper.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatMapper.java
@@ -1,4 +1,29 @@
 package com.alibou.whatsappclone.chat;
 
+
+import com.alibou.whatsappclone.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ChatMapper {
+
+    private final UserRepository userRepository;
+
+    public ChatMapper(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public ChatResponse toChatResponse(Chat chat, String senderId) {
+
+
+        return ChatResponse.builder()
+                .id(chat.getId())
+                .name(chat.getChatName(senderId))
+                .unreadCount(chat.getUnreadMessages(senderId))
+                .isRecipientOnline(chat.getRecipient().isUserOnline())
+                .senderId(chat.getSender().getId())
+                .receiverId(chat.getRecipient().getId())
+                .build();
+
+    }
 }
