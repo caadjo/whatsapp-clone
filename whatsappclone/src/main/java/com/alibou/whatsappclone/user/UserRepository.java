cfc56cfdde0825014f2fa3f1dcc1bdc0f0a0f4d1
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 17:06:19 -0300

    feat: update UserRepository

--- whatsappclone/src/main/java/com/alibou/whatsappclone/user/UserRepository.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/UserRepository.java
@@ -12,7 +12,7 @@ public interface UserRepository extends JpaRepository<User,String> {
 
     @Query(name = UserConstants.FIND_USER_BY_EMAIL)
     Optional<User> findByEmail(@Param("email") String userEmail);
-    User user = userMapper.fromTokenAttributes(token.getClaims());
+    // User user = userMapper.fromTokenAttributes(token.getClaims());
 
     @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
     Optional<User> findByPublicId(String publicId);
