0d0b9ce7fd65eef9d9dbb0926a2e9fe88c45e4b0
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 17:46:58 -0300

    feat: create MessageResponse

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageResponse.java
@@ -0,0 +1,24 @@
+package com.alibou.whatsappclone.message;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class MessageResponse {
+
+    private Long id;
+    private String content;
+    private MessageType type;
+    private MessageState state;
+    private String senderId;
+    private String receiverId;
+    private LocalDateTime updatedAt;
+    private byte[] media;
+
+}
