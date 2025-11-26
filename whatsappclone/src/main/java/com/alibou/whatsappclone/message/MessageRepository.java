aa642694bbf188c2e31b5e263ad8455a0fc5454a
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 17:57:53 -0300

    feat: create setMessagesToSeenByChaId

--- whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageRepository.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageRepository.java
@@ -3,6 +3,7 @@ package com.alibou.whatsappclone.message;
 import com.alibou.whatsappclone.chat.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
@@ -13,7 +14,7 @@ public interface MessageRepository extends JpaRepository<Message,Long> {
     @Query(name = MessageConstants.FIND_MESSAGES_BY_CHAT_ID)
     List<Message> findMessagesByChatId(String chatId);
 
-
-    void setMessagesToSeenByChaId(Chat chat, MessageState messageState);
+    @Query(name = MessageConstants.SET_MESSAGES_TO_SEEN_BY_CHAT)
+    void setMessagesToSeenByChaId(@Param("chatid")String chatId, @Param("newState")MessageState state);
 
 }
