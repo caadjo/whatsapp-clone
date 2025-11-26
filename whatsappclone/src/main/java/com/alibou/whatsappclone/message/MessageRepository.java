0cf96eed16021da9801c04776e94ebed6d65629b
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-26 17:49:44 -0300

    feat: create FIND_MESSAGES_BY_CHAT_ID

--- whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageRepository.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/message/MessageRepository.java
@@ -1,7 +1,14 @@
 package com.alibou.whatsappclone.message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+import java.util.List;
 
 
 public interface MessageRepository extends JpaRepository<Message,Long> {
+
+    @Query(name = MessageConstants.FIND_MESSAGES_BY_CHAT_ID)
+    List<Message> findMessagesByChatId(String chatId);
 }
