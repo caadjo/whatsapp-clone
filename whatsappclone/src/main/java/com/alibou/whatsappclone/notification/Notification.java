63a76cbf88ece768ea92cebc8eeae2a855451cbb
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-27 15:38:28 -0300

    feat: create param

--- whatsappclone/src/main/java/com/alibou/whatsappclone/notification/Notification.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/notification/Notification.java
@@ -1,4 +1,24 @@
 package com.alibou.whatsappclone.notification;
 
+import com.alibou.whatsappclone.message.MessageType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class Notification {
+
+    private String chatId;
+    private String content;
+    private String senderId;
+    private String receiverId;
+    private String chatName;
+    private MessageType messageType;
+    private NotificationType type;
+    private byte[] image;
+
+
 }
