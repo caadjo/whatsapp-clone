ab450329465d8a54e2984a80891821b859eb8d21
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-27 15:18:30 -0300

    feat: create UserRepository

--- whatsappclone/src/main/java/com/alibou/whatsappclone/user/UserRepository.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,String> {
@@ -17,4 +19,6 @@ public interface UserRepository extends JpaRepository<User,String> {
     @Query(name = UserConstants.FIND_USER_BY_PUBLIC_ID)
     Optional<User> findByPublicId(String publicId);
 
+    @Query(UserConstants.FIND_ALL_USER_EXCEPT_SELF)
+    List<User>findAllUserExceptSelf(@Param("publicId") String senderId);
 }
