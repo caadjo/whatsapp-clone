9f45928da1a78cab4bb8b7453042342e45625470
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-27 20:08:04 -0300

    feat: update wsc

--- whatsappclone/src/main/java/com/alibou/whatsappclone/ws/WebSocketConfig.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/ws/WebSocketConfig.java
@@ -6,14 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.messaging.handler.invocation.HandlerMethodArgumentResolver;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.messaging.context.AuthenticationPrincipalArgumentResolver;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -24,47 +20,36 @@ import java.util.List;
 @EnableWebSocketMessageBroker
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @RequiredArgsConstructor
-
-
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private final ObjectMapper objectMapper; // Injetado pelo Spring
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/user");
         registry.setApplicationDestinationPrefixes("/app");
-        registry.setUserDestinationPrefix("/users");
-
-
+        registry.setUserDestinationPrefix("/user");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:4200")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:5173")
                 .withSockJS();
-
-
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(new AuthenticationPrincipalArgumentResolver());
-
     }
 
     @Override
     public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-
         DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
         resolver.setDefaultMimeType(MediaType.APPLICATION_JSON);
+        
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-        converter.setObjectMapper(new ObjectMapper());
+        // Usar o ObjectMapper configurado pelo Spring, que já conhece o JSR310
+        objectMapper.findAndRegisterModules(); // Garante que todos os módulos, incluindo o de data/hora, estão registrados
+        converter.setObjectMapper(objectMapper);
         converter.setContentTypeResolver(resolver);
+        
         messageConverters.add(converter);
-
-
         return false;
-
     }
 }
