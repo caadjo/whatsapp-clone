9b970354b346d75368c2bdb2817cef947912aad5
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-27 14:41:46 -0300

    feat: create FileService.java

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/file/FileService.java
@@ -0,0 +1,10 @@
+package com.alibou.whatsappclone.file;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileService {
+
+    public String saveFile(MultipartFile file, String senderId) {
+        return null;
+    }
+}
