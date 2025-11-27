419f81e5b585afb710b8322c3883df714ed82848
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-27 16:22:31 -0300

    feat: update User.java

--- whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/user/User.java
@@ -2,17 +2,19 @@ package com.alibou.whatsappclone.user;
 
 import com.alibou.whatsappclone.chat.Chat;
 import com.alibou.whatsappclone.common.BaseAuditingEntity;
-import jakarta.persistence.*;
-import jdk.jfr.Name;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-
 
 @Getter
 @Setter
@@ -20,17 +22,16 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-
 @NamedQuery(name = UserConstants.FIND_USER_BY_EMAIL,
-            query = "SELECT u FROM  User u WHERE u.email = :email")
-@NamedQuery(name = UserConstants.FIND_ALL_USER_EXCEPT_SELF,
-            query = "SELECT u  FROM User u WHERE u.id !=:publicId")
-
+        query = "SELECT u FROM User u WHERE u.email = :email"
+)
+@NamedQuery(name = UserConstants.FIND_ALL_USERS_EXCEPT_SELF,
+        query = "SELECT u FROM User u WHERE u.id != :publicId")
 @NamedQuery(name = UserConstants.FIND_USER_BY_PUBLIC_ID,
-            query = "SELECT u  FROM User u WHERE u.id =:publicId")
-
-
+        query = "SELECT u FROM User u WHERE u.id = :publicId")
 public class User extends BaseAuditingEntity {
+
+    private static final int LAST_ACTIVATE_INTERVAL = 5;
 
     @Id
     private String id;
@@ -39,20 +40,15 @@ public class User extends BaseAuditingEntity {
     private String email;
     private LocalDateTime lastSeen;
 
-
     @OneToMany(mappedBy = "sender")
-    private List<Chat> chatAsSender;
-    @OneToMany(mappedBy = "recipient")
-    private List<Chat> chatAsRecipient;
+    private List<Chat> chatsAsSender;
 
+    @OneToMany(mappedBy = "recipient")
+    private List<Chat> chatsAsRecipient;
 
     @Transient
-    public boolean isUserOnline(){
-    return lastSeen != null && lastSeen.isAfter(LocalDateTime.now().minusMinutes(LAST_ACTIVE_INTERVAL));
-
-
+    public boolean isUserOnline() {
+        return lastSeen != null && lastSeen.isAfter(LocalDateTime.now().minusMinutes(LAST_ACTIVATE_INTERVAL));
     }
-
-
 
 }
