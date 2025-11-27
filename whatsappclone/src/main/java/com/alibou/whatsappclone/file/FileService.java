a28ae00bb1c04860d66635130f196e67872deb43
Caio <cvd@aluno.ifnmg.edu.br>
2025-11-27 15:01:29 -0300

    fix: @value filepath

--- whatsappclone/src/main/java/com/alibou/whatsappclone/file/FileService.java
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/file/FileService.java
@@ -1,9 +1,19 @@
 package com.alibou.whatsappclone.file;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 
 public class FileService {
 
+    @Value("${application.file.uploads.media-output.path}")
+    private String fileUploadPath;
     public String saveFile(MultipartFile file, String senderId) {
         return null;
     }
